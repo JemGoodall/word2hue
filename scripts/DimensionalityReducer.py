refactor of scripts, fixed percentile calculation

scripts/DimensionalityReducer.py
@@ -13,7 +13,7 @@ class DimensionalityReducer():
     def reduce(self, data, components:int):
         match self.reduction_method:
             case 'quartiles':
-                reduced_data = self.quartiles(data, components)
+                reduced_data = self.quartiles(data)
             case 'pca':
                 reduced_data = self.pca(data, components)
             case 'tsne':
@@ -24,15 +24,17 @@ class DimensionalityReducer():
 
     def quartiles(self, data):
         """ Takes numpy array, returns numpy array of Q1, mean, Q3"""
-        q75, q25 = np.percentile(data, [75 ,25])
-        mean = np.mean(data)
-        return np.array([abs(value) for value in [q25, mean, q75]])
+        q75, q25 = np.percentile(data, [75 ,25], axis=1)
+        mean = np.mean(data, axis=1)
+        data_stacked = np.vstack((q25, mean, q75)).T
+        return data_stacked
 
 
     def pca(self, data, components:int):
-        print('pca reducer')
+        print(data.shape)
         pca = PCA(n_components=components)
         reduced = pca.fit_transform(data)
+        print(reduced.shape)
         return reduced
 
 